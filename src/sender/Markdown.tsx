0e4abfe0e4dc4fb47342d44e889dc95493db886d
Max Wenk <max.wenk@protonmail.com>
2026-03-31 15:50:24 +0200

    fix: markdown styling

--- src/sender/Markdown.tsx
+++ src/sender/Markdown.tsx
@@ -1,28 +1,52 @@
+import './Sender.css'
 
+/**
+ * Parses Markdown into HTML elements, wheras each line is rendered independently (except for lists).
+ *
+ * Each first level heading marks a new element for the output array.
+ * This is done, as each slide should have its own description.
+ *
+ * @param text  The array of strings to parse the data from. Each item is a line from the original markdown file.
+ * @return An array of strings where each item contains HTML.
+ */
 function parseMarkdown(text: string[] | null) {
     if (!text) return
     let output: string[] = []
     let currentPage = -1;
 
-    for (const line of text) {
+    for (let line of text) {
+
+        // Parse embedded links
+        line = line.replace(
+            /\[([^\]]+)\]\(([^)]+)\)/g,
+            (_match, text, url) => `<a href="${url}" target="_blank">${text}</a>`
+        );
+
+        // Parse first level heading -> e.g. creates new page
         if (/^#\s/.test(line)) {
             currentPage++;
 
             const match = line.match(/^#\s(.*)/);
             const content = match ? match[1] : "";
 
-            output[currentPage] = `<h2>${content}</h2>`;
-        } else if (/^##\s/.test(line)) {
+            output[currentPage] = `<h3>${content}</h3>`;
+        }
+        // Parse second level heading
+        else if (/^##\s/.test(line)) {
             const match = line.match(/^##\s(.*)/);
             const content = match ? match[1] : "";
 
-            output[currentPage] += `<h3>${content}</h3>`;
-        } else if (/^###\s/.test(line)) {
+            output[currentPage] += `<h4>${content}</h4>`;
+        }
+        // Parse third level heading
+        else if (/^###\s/.test(line)) {
             const match = line.match(/^###\s(.*)/);
             const content = match ? match[1] : "";
 
-            output[currentPage] += `<h4>${content}</h4>`;
-        } else if (/^\*\s/.test(line)) {
+            output[currentPage] += `<h5>${content}</h5>`;
+        }
+        // Parse unordered list
+        else if (/^\*\s/.test(line)) {
             const match = line.match(/^\*\s(.*)/);
             const content = match ? match[1] : "";
 
@@ -33,7 +57,9 @@ function parseMarkdown(text: string[] | null) {
             } else {
                 output[currentPage] += `<ul><li>${content}</li></ul>`
             }
-        } else {
+        }
+        // If non of the above items has matched yet, the line is a paragraph by default
+        else {
             if (currentPage === -1) {
                 currentPage = 0;
                 output[currentPage] = "";
@@ -46,11 +72,22 @@ function parseMarkdown(text: string[] | null) {
     return output
 }
 
+/**
+ * Renders Markdown as HTML.
+ *
+ * The rendered Markdown is dependant on the current slide.
+ * Each slide can have its own notes (for more information see README).
+ *
+ * @param param0            Default parameter.
+ * @param param0.markdown   The markdown file as a array of strings, where each element is a line.
+ * @param param0.pageNumber The page number of the currently displayed slide.
+ * @constructor
+ */
 function MarkdownRenderer({markdown, pageNumber}: {markdown: string[] | null, pageNumber: number}) {
     const pageContent = parseMarkdown(markdown)
 
     return (
-        <div dangerouslySetInnerHTML={{ __html: pageContent ? pageContent[Math.max(pageNumber - 1, 0)] || "" : "" }} />
+        <div className={"markdown-to-html"} dangerouslySetInnerHTML={{ __html: pageContent ? pageContent[Math.max(pageNumber - 1, 0)] || "" : "" }} />
     )
 }
 
